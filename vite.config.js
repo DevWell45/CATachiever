76fab3c1d47d27e25ecf28807a255cd9393cb4b7
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-05 23:38:50 +0800

    added new inputs in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js',
                 'resources/js/functions/Auth/logout.js', 'resources/js/functions/pages/Home.js', 
-                'resources/js/functions/OTP_Verification_Page/Resend_OTP.js', 'resources/js/functions/OTP_Verification_Page/OTP_inputs.js'],
+                'resources/js/functions/OTP_Verification_Page/Resend_OTP.js', 'resources/js/functions/OTP_Verification_Page/OTP_inputs.js',
+            'resources/css/pages/exam.css'],
             refresh: true,
         }),
         vue(),
