3997e4205153a3575c723d83a39b28f0a2c309b4
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-05 23:21:21 +0800

    added new inputs in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/functions/Auth/Logout.js', 'resources/js/functions/pages/Home.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js',
+                'resources/js/functions/Auth/Logout.js', 'resources/js/functions/pages/Home.js', 
+                'resources/js/functions/OTP_Verification_Page/Resend_OTP.js', 'resources/js/functions/OTP_Verification_Page/OTP_inputs.js'],
             refresh: true,
         }),
         vue(),
