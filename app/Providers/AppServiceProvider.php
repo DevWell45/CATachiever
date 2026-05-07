f4a0c0016390e7b5688336aac24271265ce7754a
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-07 21:33:21 +0800

    fixed AppserviceProvide.php boot()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,20 +24,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
 
-        // --- ADD THIS BLOCK ---
-        Mail::extend('mailjet', function (array $config) {
-            return (new MailjetTransportFactory)->create(
-                new Dsn(
+        \Illuminate\Support\Facades\Mail::extend('mailjet', function () {
+            // This line cleans the key: it removes quotes, newlines, and spaces
+            $apiKey = str_replace(['"', "\n", "\r", " "], '', env('BREVO_API_KEY'));
+
+            return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
+                new \Symfony\Component\Mailer\Transport\Dsn(
                     'mailjet+api',
                     'default',
-                    $config['key'],
-                    $config['secret'] ?? 'not-needed'
+                    $apiKey
                 )
             );
         });
-        // ----------------------
     }
 }
