cbae662cb4c0c77543e0d3b259052dbc3d91f55a
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-08 21:40:10 +0800

    fixed AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,19 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        \Illuminate\Support\Facades\Mail::extend('mailjet', function () {
+        // This is the "Secret Sauce": It forces the key into one line 
+        // and removes the quotes that Railway is stuck on.
+        $apiKey = str_replace(['"', "\n", "\r", " "], '', env('BREVO_API_KEY'));
+
+        return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
+            new \Symfony\Component\Mailer\Transport\Dsn(
+                'mailjet+api',
+                'default',
+                $apiKey
+            )
+        );
+    });
     }
 }
