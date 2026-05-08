cefd2f4329525f8dd4f109b24a85ceb8dba3201b
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-08 22:29:52 +0800

    changed mailjet

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,25 +23,30 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    if (app()->environment('production')) {
-        \URL::forceScheme('https');
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        Mail::extend('brevo', function () {
+
+            $rawKey = env('BREVO_API_KEY');
+
+            // Remove unwanted characters
+            $apiKey = str_replace(
+                ['"', "\n", "\r", " "],
+                '',
+                (string) $rawKey
+            );
+
+            return (new BrevoTransportFactory)->create(
+                new Dsn(
+                    'brevo+api',
+                    'default',
+                    'apikey',
+                    $apiKey
+                )
+            );
+        });
     }
-
-    \Illuminate\Support\Facades\Mail::extend('mailjet', function () {
-        // Pull the exact variable you have in Railway
-        $rawKey = env('BREVO_API_KEY');
-        
-        // Clean it: remove quotes, newlines, and spaces
-        $apiKey = str_replace(['"', "\n", "\r", " "], '', (string)$rawKey);
-
-        return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
-            new \Symfony\Component\Mailer\Transport\Dsn(
-                'mailjet+api',
-                'default',
-                $apiKey // Symfony treats this third parameter as the "Password"
-            )
-        );
-    });
-}
 }
