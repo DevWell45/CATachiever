10060ea5c572021d2959592f37365e6d1965c718
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-08 22:20:05 +0800

    added mailjet in php/mail.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,25 +19,25 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        if (app()->environment('production')) {
-            \URL::forceScheme('https');
-        }
-
-        \Illuminate\Support\Facades\Mail::extend('mailjet', function () {
-            // Try to get the key from either variable name just in case
-            $rawKey = env('BREVO_API_KEY') ?? env('MAIL_PASSWORD');
-            
-            // Clean the key
-            $apiKey = str_replace(['"', "\n", "\r", " "], '', (string)$rawKey);
-
-            return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
-                new \Symfony\Component\Mailer\Transport\Dsn(
-                    'mailjet+api',
-                    'default',
-                    $apiKey // Symfony throws "Password not set" if this is empty
-                )
-            );
-        });
+{
+    if (app()->environment('production')) {
+        \URL::forceScheme('https');
     }
+
+    \Illuminate\Support\Facades\Mail::extend('mailjet', function () {
+        // Pull the exact variable you have in Railway
+        $rawKey = env('BREVO_API_KEY');
+        
+        // Clean it: remove quotes, newlines, and spaces
+        $apiKey = str_replace(['"', "\n", "\r", " "], '', (string)$rawKey);
+
+        return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
+            new \Symfony\Component\Mailer\Transport\Dsn(
+                'mailjet+api',
+                'default',
+                $apiKey // Symfony treats this third parameter as the "Password"
+            )
+        );
+    });
+}
 }
