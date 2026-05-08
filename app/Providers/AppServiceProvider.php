e8225065485ffa497bfe605695a4909be05a3a6c
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-08 22:15:01 +0800

    added mailjet in php/mail.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,21 +21,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
 
         \Illuminate\Support\Facades\Mail::extend('mailjet', function () {
-        // This is the "Secret Sauce": It forces the key into one line 
-        // and removes the quotes that Railway is stuck on.
-        $apiKey = str_replace(['"', "\n", "\r", " "], '', env('BREVO_API_KEY'));
+            // Try to get the key from either variable name just in case
+            $rawKey = env('BREVO_API_KEY') ?? env('MAIL_PASSWORD');
+            
+            // Clean the key
+            $apiKey = str_replace(['"', "\n", "\r", " "], '', (string)$rawKey);
 
-        return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
-            new \Symfony\Component\Mailer\Transport\Dsn(
-                'mailjet+api',
-                'default',
-                $apiKey
-            )
-        );
-    });
+            return (new \Symfony\Component\Mailer\Bridge\Mailjet\Transport\MailjetTransportFactory)->create(
+                new \Symfony\Component\Mailer\Transport\Dsn(
+                    'mailjet+api',
+                    'default',
+                    $apiKey // Symfony throws "Password not set" if this is empty
+                )
+            );
+        });
     }
 }
