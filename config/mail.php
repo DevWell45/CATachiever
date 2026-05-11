8568d1d61e8eedc4b62a4e3aaec4bf148cd41355
Jimwell Raza <jimwellraza005@gmail.com>
2026-05-11 19:49:39 +0800

    added local_domain in bervo mailer in config/mail.php

--- config/mail.php
+++ config/mail.php
@@ -131,6 +131,10 @@ return [
         ],
         'brevo' => [
             'transport' => 'brevo',
+            'local_domain' => env(
+                'MAIL_EHLO_DOMAIN',
+                parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)
+            ),
         ],
 
     ],
